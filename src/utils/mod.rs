9f4f835605cce8f9cae34ab8c557eb9a8fb52a7a
TheRawMeatball <therawmeatball@gmail.com>
2021-10-29 14:12:14 +0300

    codedump

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -11,7 +11,7 @@ use egui::{Color32, Label};
 
 const ERROR_COLOR: Color32 = Color32::from_rgb(255, 0, 0);
 
-pub(crate) fn error_label(ui: &mut egui::Ui, msg: impl Into<Label>) {
+pub(crate) fn error_label(ui: &mut egui::Ui, msg: impl ToString) {
     ui.colored_label(ERROR_COLOR, msg);
 }
 
